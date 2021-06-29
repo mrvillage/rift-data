4abd5a799cf7fd563dab1c4614d1cf69c055ab6f
Village05 <mr.village05@gmail.com>
2021-06-28 23:06:33 -0400

    Removed print from dispatch

--- source/events/__init__.py
+++ source/events/__init__.py
@@ -4,5 +4,4 @@ from ..env import SERVER
 
 
 async def dispatch(event: str, time: Union[str, datetime], **kwargs):
-    print({"event": event, "time": time, "data": kwargs})
     await SERVER.send_all({"event": event, "time": time, "data": kwargs})
