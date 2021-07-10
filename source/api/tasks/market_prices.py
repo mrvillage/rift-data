bda5d268465b7af069a5895f3e22b82746ec7192
Village05 <mr.village05@gmail.com>
2021-07-09 23:36:26 -0400

    Fixed another issue

--- source/api/tasks/market_prices.py
+++ source/api/tasks/market_prices.py
@@ -32,7 +32,7 @@ async def fetch_market_prices():
     """
     async with aiohttp.request("GET", GQLURL, json={"query": query}) as response:
         data = await response.json()
-        data = data["data"]["tradeprices"]
+        data = data["data"]["tradeprices"][0]
         old = await execute_read_query(
             """
             SELECT credit, coal, oil, uranium,
