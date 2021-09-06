7a8e896720b145dd9df905d09327d851c881fb79
mrvillage <mr.village05@gmail.com>
2021-09-06 00:07:03 -0400

    Disable websocket max message size

--- src/server/__init__.py
+++ src/server/__init__.py
@@ -44,7 +44,7 @@ class SocketServer:
         return decorator
 
     async def socket_handler(self, req: BaseRequest) -> None:
-        websocket = web.WebSocketResponse()
+        websocket = web.WebSocketResponse(max_msg_size=0)
         await websocket.prepare(req)
         self.sockets.append(
             {
